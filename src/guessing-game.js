664adbf7890c9fe214423662c11fc6a9a64a7433
EvgenKh <hamitcevich@gmail.com>
2024-09-16 16:16:34 +0300

    feat: solution task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.value = null;
+        this.left = 0;
+        this.right = 0;
+    }
 
     setRange(min, max) {
-
+        this.left = min;
+        this.right = max;
     }
 
     guess() {
-
+        this.value = Math.ceil(this.left + ((this.right - this.left) / 2));
+        return this.value;
     }
 
     lower() {
-
+        // this.left;
+        this.right = this.value;
     }
 
     greater() {
-
+        this.left = this.value;
+        // this.right;
     }
 }
 
